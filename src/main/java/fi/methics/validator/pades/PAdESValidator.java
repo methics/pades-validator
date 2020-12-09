7e71759d66ba2d938554ebe7ed7e1585025a1c8f
Eemeli Miettinen <hemuli130@gmail.com>
2020-12-09 12:50:43 +0200

    Clean up imports

--- src/main/java/fi/methics/validator/pades/PAdESValidator.java
+++ src/main/java/fi/methics/validator/pades/PAdESValidator.java
@@ -7,12 +7,9 @@ import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Enumeration;
-
-import org.bouncycastle.cms.CMSException;
 
 import eu.europa.esig.dss.alert.LogOnStatusAlert;
 import eu.europa.esig.dss.model.DSSDocument;
